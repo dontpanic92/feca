3ed5dd1066a964d524244b32be54ed51269f1b63
Shengqiu Li <lishengqiu.hit@gmail.com>
2022-08-31 08:52:42 +0000

    Fix img color

--- felis/src/rendering/cairo.rs
+++ felis/src/rendering/cairo.rs
@@ -1,6 +1,4 @@
-use std::io::Cursor;
-
-use cairo::{Context, FontFace, ImageSurface, Pattern, Win32Surface};
+use cairo::{Context, Win32Surface};
 use image::RgbaImage;
 use raw_window_handle::{HasRawWindowHandle, Win32WindowHandle};
 use winapi::shared::windef::HWND;
@@ -49,20 +47,33 @@ impl CairoRenderer {
         self.context.restore().unwrap();
     }
 
-    pub fn render_png(&self, boundary: &Rectangle, mut img: RgbaImage) {
+    pub fn render_png(&self, boundary: &Rectangle, img: RgbaImage) {
         self.context.save().unwrap();
 
         let width = img.width();
         let height = img.height();
-        let buf = img.as_mut_ptr();
+        let stride = cairo::Format::ARgb32.stride_for_width(width).unwrap();
+        let buf_len = (stride * height as i32) as usize;
+        let mut buffer = Vec::<u8>::with_capacity(buf_len);
+        buffer.resize(buf_len, 0);
+
+        for (i, row) in img.rows().enumerate() {
+            let row_start = i * stride as usize;
+            for (k, p) in row.enumerate() {
+                buffer[row_start + k * 4 + 0] = p.0[2];
+                buffer[row_start + k * 4 + 1] = p.0[1];
+                buffer[row_start + k * 4 + 2] = p.0[0];
+                buffer[row_start + k * 4 + 3] = p.0[3];
+            }
+        }
         {
             let img = unsafe {
                 cairo::ImageSurface::create_for_data_unsafe(
-                    buf,
+                    buffer.as_mut_ptr(),
                     cairo::Format::ARgb32,
                     width as i32,
                     height as i32,
-                    (width * 4) as i32,
+                    stride as i32,
                 )
                 .unwrap()
             };
